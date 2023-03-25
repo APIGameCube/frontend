67d8794150d4d07a9dcf621270b9bb971b838382
AlaaHmaidat <alaahmidat2000@gmail.com>
2023-03-25 09:45:36 +0000

    start in musicRoute

--- src/componants/Footer/Footer.js
+++ src/componants/Footer/Footer.js
@@ -6,9 +6,10 @@ function Footer() {
     return (
         <div className="foot">
         <div className="Footer">
-            <p> &copy; all copy right are resarved </p>
+            <p> all copy right are resarved </p>
             <p>contact us on Social media:</p>
             <ul className='buttons'>
+               
                 <li>
                     <a href="https://github.com/Group04-Musicly" target="_blank" rel="noreferrer"><i className="fab fa-github"><FiGithub /></i></a>
                 </li>
